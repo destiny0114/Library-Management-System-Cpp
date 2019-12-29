fe3a6166a8276862e83cff5d4ab21cddf73978d8
destiny0114 <42262712+destiny0114@users.noreply.github.com>
2019-12-29 11:45:45 +0800

    Initial Commit

---
+++ Library Management System C++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Library Management System C++
+//
+//  Created by Rockabye Saw on 29/12/2019.
+//  Copyright © 2019 Rockabye Saw. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
